448cbdd7e6886cc6c074a6c4e11c400d4390cec0
TenCreator <kk@tencreator.xyz>
2024-11-08 18:16:59 +0000

    fixed layout.tsx shitting itself upon build

--- src/app/staff/layout.tsx
+++ src/app/staff/layout.tsx
@@ -1,10 +1,16 @@
-import { hasPermission, type PagePermissions} from "@/utils/permissions"
+import { hasPermission, type PagePermissions } from "@/utils/permissions"
 import { auth } from "@/utils/auth"
 
 import Sidebar from "@/components/staff/sidebar"
 import Login from "@/components/auth/login"
 
-export default async function StaffLayout({ children, perm, redirect } : { children: React.ReactNode, perm: PagePermissions, redirect?: string }) {
+interface StaffLayoutProps {
+    children: React.ReactNode;
+    perm: PagePermissions;
+    redirect?: string;
+}
+
+const StaffLayout = async ({ children, perm, redirect }: StaffLayoutProps) => {
     const session = await auth()
 
     if (!session) return <Login redirect={redirect} />
@@ -19,3 +25,11 @@ export default async function StaffLayout({ children, perm, redirect } : { child
         </section>
     )
 }
+
+const withAsync = (Component: any) => {
+    return (props: any) => (
+        <Component {...props} />
+    )
+}
+
+export default withAsync(StaffLayout)

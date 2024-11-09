d63ece138944e0654cb904175439a9c7247a0e65
Luca Kinsey <lucakinsey@gmail.com>
2024-11-08 22:31:32 -0500

    I see my issue
    I am stupid and used setTimeout instead of setInterval and it was just updating every time the data state was changed -w-

--- src/components/staff/stats/stats.tsx
+++ src/components/staff/stats/stats.tsx
@@ -18,14 +18,10 @@ export default function Stats() {
 
     useEffect(() => {
         fetchStats()
-        const timeout = setTimeout(()=>{
+        setInterval(()=>{
             fetchStats()
         }, 2500)
-
-        return () => {
-            clearTimeout(timeout)
-        }
-    }, [data])
+    }, [])
 
     return (
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">

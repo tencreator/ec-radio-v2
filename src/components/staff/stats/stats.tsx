35b985cdc52fa18cdb1e8b89dd6c47ffd8051726
Luca Kinsey <lucakinsey@gmail.com>
2024-11-08 22:21:08 -0500

    Fixed cards not auto updating

--- src/components/staff/stats/stats.tsx
+++ src/components/staff/stats/stats.tsx
@@ -18,8 +18,14 @@ export default function Stats() {
 
     useEffect(() => {
         fetchStats()
-        setTimeout(()=>{fetchStats()}, 1000)
-    }, [])
+        const timeout = setTimeout(()=>{
+            fetchStats()
+        }, 1000)
+
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [data])
 
     return (
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">

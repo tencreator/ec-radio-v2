f4abaa46d2016fd4cf612b2433fd00ab24de235e
TenCreator <kk@tencreator.xyz>
2024-11-12 18:06:06 +0000

    Added image optimisation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.emeraldcoastrp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discord.com",
+      }
+    ]
+  }
 };
 
 export default nextConfig;

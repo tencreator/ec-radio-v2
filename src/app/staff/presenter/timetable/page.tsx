fb90e24c120018382fbd8c1db8dc52793b8ee6ae
TenCreator <kk@tencreator.xyz>
2024-11-18 14:36:49 +0000

    viewer no-longer makes requests while hidden

--- src/app/staff/presenter/timetable/page.tsx
+++ src/app/staff/presenter/timetable/page.tsx
@@ -1,16 +1,16 @@
 import { Permissions } from "@/utils/permissions"
-import Layout from "@/app/staff/layout"
 import { Suspense } from "react"
-
 import { headers, cookies } from "next/headers"
+import Layout from "@/app/staff/layout"
+
 
 export default async function Page() {
     return (
         <div className="mx-auto mt-4 w-10/12 lg:w-11/12">
             <Suspense fallback={<div>Loading...</div>}>
                 <div>
-                    <h1 className="text-3xl font-semibold">Connection</h1>
-                    <p className="text-sm text-gray-500">Get your connection details below!</p>
+                    <h1 className="text-3xl font-semibold">Timetable</h1>
+                    <p className="text-sm text-gray-500">Get to booking your preferred timeslots below!</p>
                 </div>
 
 
@@ -21,4 +21,4 @@ export default async function Page() {
 
 
 
-Page.getLayout = (page: any) => <Layout perm={Permissions.SELF_CONNECTION}>{page}</Layout>
+Page.getLayout = (page: any) => <Layout perm={Permissions.VIEW_TIMETABLE}>{page}</Layout>

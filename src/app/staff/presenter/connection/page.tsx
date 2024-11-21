bcc90237a40af41ffde81a4d1c2d1ce248ee3204
TenCreator <lucakinsey@gmail.com>
2024-11-21 16:12:18 -0500

    Update page.tsx

--- src/app/staff/presenter/connection/page.tsx
+++ src/app/staff/presenter/connection/page.tsx
@@ -1,10 +1,16 @@
-import { Permissions } from "@/utils/permissions"
+import { hasPermission, Permissions } from "@/utils/permissions"
+import { redirect } from "next/navigation"
 import Layout from "@/app/staff/layout"
 import { Suspense } from "react"
 
 import ViewDetails from "@/components/staff/presenters/connection/ViewDetails"
 
 export default async function Page() {
+    const session = await auth()
+
+    if (!session || !session.user || !session.user.providerId) redirect('/auth')
+    if (!await hasPermission(session.user.providerId, Permissions.VIEW_STATS)) return <div>Unauthorized</div>
+
     return (
         <div className="mx-auto mt-4 w-10/12 lg:w-11/12">
             <Suspense fallback={<div>Loading...</div>}>
